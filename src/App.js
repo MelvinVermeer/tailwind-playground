af633ad52a91901b7d5b825841ca1981e07e028b
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-08-20 20:59:09 +0200

    use tailwind classes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-gray-500">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} alt="logo" />
+        <p className="text-white text-center">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="hover:text-white"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
